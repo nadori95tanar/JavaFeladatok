7582d44ae57d9f25efe6ec1ec0ee5c08f52ab02b
Nádori Sándor <nadori.sandor@osztalyterem.katolikuskeri.hu>
2022-02-23 10:10:37 +0100

    Az 1. feladat megoldása.

---
+++ feladat1/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /* Feladat: Írj programot, ami kiírja a képernyőre, hogy "Hello world!"!*/
+        /*
+            A System.out.println() metódus segítségével értéket tudunk
+            kiírni a konzolra, úgy hogy a kiírt érték után egy sortörést
+            (Enter-t) is rak!
+         */
+        System.out.println("Hello World");
+    }
+}
